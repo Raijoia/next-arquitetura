888d026bac4595fdacefb920bdd89780e989c52d
Rai Joia <megac154@gmail.com>
2023-10-23 17:04:04 -0300

    packages in project

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,10 @@
+import { sum } from "@rai/utils/math/sum"
+
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>Importando modulo local: @rai/utils/math/sum sum(2, 2): {sum(2, 2)} </p>
     </main>
   );
 }
