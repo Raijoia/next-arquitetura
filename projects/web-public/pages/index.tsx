eb7f4e706d9f205e08fd6b810493d5cccbb0cbb7
Rai Joia <megac154@gmail.com>
2023-10-24 09:52:26 -0300

    add components project with arquiteture monorepo

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,10 +1,11 @@
+import { Text } from "@rai/design-system/components/Text"
 import { sum } from "@rai/utils/math/sum"
 
 export default function HomeScreen() {
   return (
     <main>
-      <h1>Home</h1>
-      <p>Importando modulo local: @rai/utils/math/sum sum(2, 2): {sum(2, 2)} </p>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">Importando modulo local: @rai/utils/math/sum sum(2, 2): {sum(2, 2)} </Text>
     </main>
   );
 }
