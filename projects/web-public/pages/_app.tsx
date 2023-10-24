fb76b5b50af19bccc84ece50429086a6cec2c9b1
Rai Joia <megac154@gmail.com>
2023-10-24 12:28:58 -0300

    styled-components with globalStyle

---
+++ projects/web-public/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme, createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
